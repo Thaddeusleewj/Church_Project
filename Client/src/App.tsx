db2ea076161236a483cd38191a6bcc937a942ec9
Thaddeus Lee <thaddeuslee@Thaddeuss-MacBook-Pro.local>
2023-10-31 00:58:21 +0800

    Initial commit to set up project

---
+++ Client/src/App.tsx
@@ -0,0 +1,16 @@
+import { Button } from './components/ui/button'
+
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <Button>
+        <span>Click me</span>
+      </Button>
+    </>
+  )
+}
+
+export default App
